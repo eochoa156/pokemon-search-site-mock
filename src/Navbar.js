96fd0fc6450e83ae5284af7e3c3a02765bd6a158
Emily Ochoa <eochoa156@gmail.com>
2024-05-29 09:22:56 -0700

    made Navbar.js

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import  AppBar from "@mui/material/AppBar";
+
+function Navbar () {
+    return (
+        <div>
+            <AppBar>
+                hi seacrhing here
+            </AppBar>
+        </div>
+    )
+};
+
+export default Navbar;
